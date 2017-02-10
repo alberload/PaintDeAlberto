3895749deecb18d62b28d4729a87387c57116dfb
xp <xp@DESKTOP-2D3CPFP>
2017-02-10 08:44:48 +0100

    Triangulo equilatero ok

--- src/codigo/Triangulo.java
+++ src/codigo/Triangulo.java
@@ -21,17 +21,13 @@ public class Triangulo extends Polygon{
     public Triangulo (int _posX, int _posY, int _altura, Color _color, boolean _relleno){
         //número de puntos que tiene el polígono
         this.npoints = 3;
-        
 
-        
         this.xpoints[0] = _posX;
         this.ypoints[0] = _posY;
         this.ypoints[1] = _posY + _altura;
         this.ypoints[2] = _posY + _altura;
         
-
-        int mediaBase = (int) (_altura / Math.tan(Math.toRadians(60))) ;
-        
+        int mediaBase = (int) (_altura / Math.sin(Math.toRadians(60))) ;
 
         this.xpoints[1] = _posX + mediaBase;
         this.xpoints[2] = _posX - mediaBase;
@@ -46,9 +42,10 @@ public class Triangulo extends Polygon{
         int altura = Math.abs((int) ypoints[0] - posY);
         this.ypoints[1] = posY + altura;
         this.ypoints[2] = posY + altura;
-        int mediaBase = (int) (altura / Math.tan(Math.toRadians(60))) ;
+        int mediaBase = (int) (altura / Math.sin(Math.toRadians(60))) ;
         this.xpoints[1] = this.xpoints[0] + mediaBase;
         this.xpoints[2] = this.xpoints[0] - mediaBase;
+        
         g2.setColor(color);
         if (relleno){
             g2.fill(this);
